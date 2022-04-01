40ab53f1848eb52c771d6ed29415bff9edb5e89e
muhammadBilal924 <muhammadBilal924@gmail.com>
2022-04-01 10:34:33 +0500

    greeting

--- 05.type-Conversions/js/conversion.js
+++ 05.type-Conversions/js/conversion.js
@@ -1,3 +1,20 @@
 let myVar;
-myVar = 233;
-console.log(myVar)
+myVar =String (233);
+// console.log(myVar, (typeof myVar))
+
+ let booleanVar = String(true);
+//  console.log(booleanVar, (typeof booleanVar));
+
+let date = String(new Date());
+//  console.log(date, (typeof date));
+
+  let arr = String ([2, 4, 6, 9, 8, 6, 5, 1]);
+//   console.log(arr, (typeof arr));
+
+let i = 43;
+//  console.log,(i.toString());
+  
+let stri = String("434242");
+// stri = Number(true);
+console.log =(stri, (typeof stri)); 
+
---
+++ 06.string-concadination/js/string.js
@@ -0,0 +1,4 @@
+console.log('we are at tut 6');
+const name = 'Harry';
+const greeting = 'Good Morning';
+console.log(greeting + ' ' + name);
