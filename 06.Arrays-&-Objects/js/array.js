78e755ef8e3fcaa47cd59878365a77ca4547a066
muhammadBilal924 <muhammadBilal924@gmail.com>
2022-04-01 16:40:33 +0500

    mixed add

--- 06.Arrays-&-Objects/js/array.js
+++ 06.Arrays-&-Objects/js/array.js
@@ -2,6 +2,10 @@ console.log('We ar in tute7.js and lets discuss about array');
 const fruits = ["Mango", "Orange", "Apple"];
 const marks = [43, 33, 56, 84, 35];
 const mixed = [43, 'str', ["mango", "Orange"]];
-const arr = new Array[67, 43, 'Orange'];
 
-console.l
+console.log(fruits[1]);
+console.log(mixed);
+//  console.log(arr);
+console.log(Array.isArray('Bilal'));
+// arr[1] = 'Bilal';
+// console.log(arr);
