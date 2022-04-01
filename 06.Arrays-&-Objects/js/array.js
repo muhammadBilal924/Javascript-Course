1280bd3f47c33b1767db176dac8058e291787071
muhammadBilal924 <muhammadBilal924@gmail.com>
2022-04-01 12:18:57 +0500

    fruits arrays

---
+++ 06.Arrays-&-Objects/js/array.js
@@ -0,0 +1,7 @@
+console.log('We ar in tute7.js and lets discuss about array');
+const fruits = ["Mango", "Orange", "Apple"];
+const marks = [43, 33, 56, 84, 35];
+const mixed = [43, 'str', ["mango", "Orange"]];
+const arr = new Array[67, 43, 'Orange'];
+
+console.l
