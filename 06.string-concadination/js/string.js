50b80ec08cf434ed6425f66bf268393393eae2e2
muhammadBilal924 <muhammadBilal924@gmail.com>
2022-04-01 12:04:19 +0500

    arays fruits

--- 06.string-concadination/js/string.js
+++ 06.string-concadination/js/string.js
@@ -2,3 +2,23 @@ console.log('we are at tut 6');
 const name = 'Harry';
 const greeting = 'Good Morning';
 console.log(greeting + ' ' + name);
+
+let html;
+html = ' <h1> this is heading </h1> ' + ' <p> this is my para </p> ';
+
+html = mtml = html.concat('this', 'str2');
+console.log(html);
+console.log(html.length);
+console.log(html.toLowerCase());
+console.log(html);
+
+console.log(html.indexOf('heading'));
+console.log(html.lastIndexOf('<'));
+console.log(html.charAt(3));
+console.log(html.endsWith('asdf'));
+console.log(html.includes('fg'));
+console.log(html.substring('1,6,7'));
+console.log(html.slice('0,5'));
+console.log(html.replace('this,it'));
+
+
