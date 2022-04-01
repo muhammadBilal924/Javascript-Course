a2ac79c53c71b7bb3afb72c655cbe17045a0af22
muhammadBilal924 <muhammadBilal924@gmail.com>
2022-04-01 18:08:34 +0500

    marks splice

--- 07.Arrays-&-Objects/js/array.js
+++ 07.Arrays-&-Objects/js/array.js
@@ -1,11 +1,19 @@
 console.log('We ar in tute7.js and lets discuss about array');
 const fruits = ["Mango", "Orange", "Apple"];
-const marks = [43, 33, 56, 84, 35];
+let marks = [43, 33, 56, 84, 35];
 const mixed = [43, 'str', ["mango", "Orange"]];
 
 console.log(fruits[1]);
 console.log(mixed);
+console.log(marks);
 //  console.log(arr);
 console.log(Array.isArray('Bilal'));
 // arr[1] = 'Bilal';
 // console.log(arr);
+
+marks.splice(1, 3);
+console.log(marks);
+ 
+let marks2 = [1, 2, 3, 4, 5]
+marks = marks.concat(marks2);
+console.log(marks);
