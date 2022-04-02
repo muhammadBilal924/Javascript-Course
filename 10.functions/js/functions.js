a808ee3036376ae90a435098ad9bc014c30083b8
muhammadBilal924 <muhammadBilal924@gmail.com>
2022-04-03 01:55:23 +0500

    manipulating window

--- 10.functions/js/functions.js
+++ 10.functions/js/functions.js
@@ -1,4 +1,10 @@
 console.log('this is tute 10');
 let name = 'Bilal';
+let name2 = 'Numan';
+let name3 = 'Sufyan';
+let name4 = 'Abdullah';
 
-console.log()
+console.log(`Happy Birthday ${name} How I wish could fly to you right now and be with you on this special day of yours`)
+console.log(`Happy Birthday ${name2} How I wish could fly to you right now and be with you on this special day of yours`)
+console.log(`Happy Birthday ${name3} How I wish could fly to you right now and be with you on this special day of yours`)
+console.log(`Happy Birthday ${name4} How I wish could fly to you right now and be with you on this special day of yours`)
---
+++ 11.Manipulating-web-usi-window-object/js/object.js
@@ -0,0 +1,3 @@
+console.log('The Manipulating websites js window objects');
+let a = window;
+console.log(a);
