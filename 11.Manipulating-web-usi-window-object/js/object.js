8a06539b8af0d50997368692d58bd28acad8faca
muhammadBilal924 <muhammadBilal924@gmail.com>
2022-04-03 03:01:33 +0500

    manipulating

--- 11.Manipulating-web-usi-window-object/js/object.js
+++ 11.Manipulating-web-usi-window-object/js/object.js
@@ -1,3 +1,10 @@
 console.log('The Manipulating websites js window objects');
-let a = window;
+// let a = window.document;
+
+// a = window.innerWidth;
+// a = window.innerHeight;
+// a = scrollX;
+// a = scrollY;
+// a = Location;
+a = window.history;
 console.log(a);
---
+++ 11.Manipulating-web-usi-window-object/js/prompt.js
@@ -0,0 +1,7 @@
+let b = Window;
+b = prompt('This will distory your computer.Type your name');
+console.log(b);
+
+
+a = confirm('Are you sure you to delete this page');
+console.log(confirm);
