f39580d2a4057d0bb93ee3b85dd31a2cc8220961
muhammadBilal924 <muhammadBilal924@gmail.com>
2022-04-02 10:17:33 +0500

    console

---
+++ 08.if-else-conditionals & switch/js/if-else.js
@@ -0,0 +1,17 @@
+console.log('this is Tutorial by 8');
+// const age = 19;
+// if (age || 19) {
+//     console.log('The Age is Defined');
+// }
+// else {
+//     console.log('The age is not defined');
+// }
+
+
+const x = 12;
+if (x == 13) {
+    console.log('This is my new paragraph');
+}
+else {
+    console.log('The move ment');
+}
