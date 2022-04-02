0a025c003a318008418c2a0a6f22eb024de171c2
muhammadBilal924 <muhammadBilal924@gmail.com>
2022-04-02 23:47:12 +0500

    do while loop

---
+++ 09.Loops/js/do-while.js
@@ -0,0 +1,17 @@
+// let i = 10;
+// do {
+//     console.log(i)
+// }
+// while (i < 10) {
+//     console.log()
+// }
+
+let k = 0;
+do {
+    if (k === 5) {
+        break;
+    }
+    console.log(k + 1);
+    k += 1;
+} while (k < 10);
+console.log('done');
--- 09.Loops/js/for-loop.js
+++ 09.Loops/js/for-loop.js
@@ -1,4 +1,9 @@
-for (let i = 0; i < 50; i++);
+console.log('We are at tutorial 9');
+for (let i = 0; i < 50; i++)
 {
-    console.log('My Name is Bilal');
+    console.log(i);
+
+}
+for (let j = 0; j < 10; j++){
+    console.log(j)
 }
