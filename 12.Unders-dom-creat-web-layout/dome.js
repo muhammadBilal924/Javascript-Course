6e2a960183611fa4e62a3cd7638e894e50c7762e
muhammadBilal924 <muhammadBilal924@gmail.com>
2022-04-03 22:01:20 +0500

    source

--- 12.Unders-dom-creat-web-layout/dome.js
+++ 12.Unders-dom-creat-web-layout/dome.js
@@ -1,6 +1,14 @@
 console.log('Welcome to tute 12.js');
 let a = document;
-a = document.all;
-a = document.body;
-a = document.forms;
+ a = document.all;
+// a = document.body;
+// a = document.forms(1);
+
+
+// Array.from(a).forEach(function(elements){
+//     console.log('element');
+// });
+// console.log(a);
+
+a = document.links;
 console.log(a);
