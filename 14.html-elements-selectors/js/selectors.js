a7ce4fb0dbc10bb0f5cef70fc4f0f21b7431385e
muhammadBilal924 <muhammadBilal924@gmail.com>
2022-04-04 03:12:01 +0500

    get class name

--- 14.html-elements-selectors/js/selectors.js
+++ 14.html-elements-selectors/js/selectors.js
@@ -3,5 +3,24 @@ console.log('Wellcome to tute 14');
 
  // multi elements selectors
 
+
+ // single elements selectors
  let element = document.getElementById('myfirst');
- console.log(element);
+//  element = element.className;
+// element = element.childNodes;
+// element = element.parentNode;
+
+
+// element.style.color = 'red';
+// element.innerText = 'Bila is a good boy';
+//  element.innerHTML = '<b> Bilal is a good boy </b>';
+// 
+
+// let sel = document.querySelector('#first');
+// sel = document.querySelector('.child');
+// sel = document.querySelector('div');
+// console.log(sel);
+
+
+let elems = document.getElementsByClassName('child');
+console.log(elems[1])
---
+++ 14.html-elements-selectors/js/warn,error.js
@@ -0,0 +1,19 @@
+
+  
+   
+        <h1>The Warn & Error Properties</h1>
+
+        // let  a = obj1,obj2,objN;
+        // console.warn(obj1 [ obj2,  objN]);
+
+
+        let marks = [1,2,3];
+        console.warn(marks);
+
+        let number = (21,34,45);
+        console.log(number);
+
+
+        
+    
+
