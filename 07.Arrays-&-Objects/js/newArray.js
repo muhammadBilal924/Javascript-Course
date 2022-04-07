52b0d5376ddf45c5649794e81ea5618316a4101a
muhammadBilal924 <muhammadBilal924@gmail.com>
2022-04-07 22:48:18 +0500

    function

--- 07.Arrays-&-Objects/js/newArray.js
+++ 07.Arrays-&-Objects/js/newArray.js
@@ -5,5 +5,30 @@
 // // const arr = new Array(34,53,345,'Orage'); 
 // console.log(fuita);
 // console.log(multi);
-// console.log(stmarks); 
+// console.log(stmarks);
+
+// unshift
+// const array = [1,2,3,4];
+// console.log(array.unshift(4,5));
+// console.log(array);
+
+// reverse
+// const array1 = ['one','two','three','four','five'];
+// console.log(array1); 
+//  DataTransferItemList.reverse;
+//  console.log(array1);
+
+ const items = [2,4,3,5,6];
+ console.log(items);
+ items.reverse;
+ console.log(items);
+
+ const array = ['a','b','c'];
+ const array2 = ['d','e','f'];
+ const array3 = array.concat(array2);
+ console.log(array3);
+ 
+
+
+
 
--- 08.if-else-conditionals & switch/js/ternaryOp.js
+++ 08.if-else-conditionals & switch/js/ternaryOp.js
@@ -1,1 +1,16 @@
-console.log(age == 19 ? 'Age is 45' : 'Age is not 24');
+console.log(age ==19? 'age is 19': 'age is not 19');
+
+// program to check pass or fail
+let marks = prompt('enter your marks:');
+// check the condition
+let result = (marks>=40)? 'pass':'fail';
+console.log(`you ${result} the exam.`);
+
+let age = 15;
+let result1 =(age>=18)? "You are eligible to vote.":"You are noy eligible for vote"
+console.log(result1);
+
+// program to check if number is positive, negative or zero
+let a = 3;
+let result = (a >= 0) ? (a == 0 ? "zero" : "positive") : "negative";
+console.log(`The number is ${result}.`);
