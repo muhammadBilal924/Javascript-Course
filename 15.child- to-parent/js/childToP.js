fd04075203419e21c93faa63c850b02b6978462d
muhammadBilal924 <muhammadBilal924@gmail.com>
2022-04-10 01:57:03 +0500

    selector

---
+++ 15.child- to-parent/js/childToP.js
@@ -0,0 +1,8 @@
+console.log('wellcome to tut with 14');
+
+ let cont = document.querySelectorAll('child');
+cont = document .querySelector('container');
+console.log(cont.childNodes); 
+
+
+
---
+++ create-elements/js/create.js
@@ -0,0 +1,5 @@
+let element = document.createElement('li');
+element.className = 'child';
+element.setAttribute('title', 'mytitle');
+let ul = document.querySelector('ul.this');
+console.log(element);
